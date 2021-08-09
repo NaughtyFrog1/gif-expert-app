07b4acf536286962229cee91df5a3bf81e7088fb
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-08-09 00:24:15 -0300

    create GifExpertApp component

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    'One Punch',
+    'Samurai X',
+    'Dragon Ball',
+  ])
+
+  const handleAdd = () => {
+    setCategories([...categories, 'Hunter X Hunter'])
+  }
+
   return (
     <>
       <h2>GifExpertApp</h2>
-      <hr/>
+      <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   )
 }
 
 export default GifExpertApp
+
+/**
+ * Cada elemento en una lista debe tener una "key" prop única.
+ *
+ * Se recomienda que esta "key" no sea el índice del elemento, ya que si se modifica el array pueden producirse errores
+ */
